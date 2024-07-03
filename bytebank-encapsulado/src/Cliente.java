7f4427996b9b824b619fc09fecc1ca194e95d749
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-03 15:32:18 -0300

    Pequena refatoração

--- bytebank-encapsulado/src/Cliente.java
+++ bytebank-encapsulado/src/Cliente.java
@@ -1,12 +1,9 @@
-public class Funcionario {
+
+public class Cliente {
 
 	private String nome;
 	private String cpf;
-	private double salario;
-	
-	public double getBonificacao() {
-		return this.salario * 0.1;
-	}
+	private String profissao;
 	
 	public String getNome() {
 		return nome;
@@ -20,12 +17,12 @@ public class Funcionario {
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	public double getSalario() {
-		return salario;
+	public String getProfissao() {
+		return profissao;
 	}
-	public void setSalario(double salario) {
-		this.salario = salario;
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
 	}
-	
 	
 }
+
