372884121fd29c0296c98431d968e56b1609eedc
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-07-08 09:17:50 -0300

    A primeira interface

--- bytebank-herdado/src/Autenticavel.java
+++ bytebank-herdado/src/Autenticavel.java
@@ -1,17 +1,9 @@
 
-public abstract class Autenticavel {
+//contrato Autenticavel
+	//quem assinar esse contrato, precisa implementar
+		//metodo setSenha e autentica
+public abstract interface Autenticavel {
 	
-	private int senha;
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public boolean autentica(int senha) {
-		if (this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+	public abstract void setSenha(int senha);
+	public abstract boolean autentica(int senha);
 }
--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,9 +1,26 @@
-//Gerente é um funcionarioAutentucavel, Gerente herda da classe FuncionarioAutenticavel
-public class Gerente extends Funcionario {
+//Gerente é um funcionario, Gerente herda da classe Funcionario, assina o contrato autenticavel, é um Autenticavel
+public class Gerente extends Funcionario implements Autenticavel {
+	
+	private int senha;
 	
 	public double getBonificacao() {
 		System.out.println("Chamando o método de bonificacao do GERENTE");
 		return super.getSalario();
 	}
-	
+
+	@Override
+	public void setSenha(int senha) {
+		this.senha = senha;
+		
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+	}
+
 }
